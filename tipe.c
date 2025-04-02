35a9f1050d536cfbf95021d9c13bd41542723cad
Valentin Forgerit <v.forgerit@gmail.com>
2025-04-02 11:59:24 +0200

    le free n'a plus de probleme mais mon code me renvois un valeurs indesirable

--- tipe.c
+++ tipe.c
@@ -5,6 +5,7 @@
 struct arbre
 {
   int score;
+  float score_fin;
   int carte_nous;
   int carte_adv;
   struct arbre *enfants[16];
@@ -50,7 +51,7 @@ arbre *init_arbre(int x,int y,int *tab1,int *tab2)
  return a;
 }
 
-void init_score(arbre *a,int score_pred)
+void init_score(arbre *a,float score_pred)
 {
   if (a==NULL)
     return;
@@ -71,19 +72,54 @@ void init_score(arbre *a,int score_pred)
 return;
 }
 
-void free_arbre(arbre **addr_a)
+void remonte(arbre *a)
 {
- arbre *a = *addr_a;
- if (addr_a == NULL)
-   {free(addr_a);
-    return;}
+ if (a==NULL)
+  return;
  int i;
+ float acc=0.0;
+ int tmp=0;
+ for (i=0;i<16;i=i+1)
+  {
+    if (a->enfants[i]!=NULL)
+      {
+       remonte(a->enfants[i]);
+       acc = acc + a->enfants[i]->score_fin;
+       tmp = tmp+1;
+       acc=acc/tmp;
+       a->score_fin = acc;
+      }
+  }
+  if (acc ==0.0)
+    a->score_fin = (float)a->score;
+  return;
+}
+
+int reponce(arbre*a)
+{
+  int i;
+  float max = -6.0;
+  int res=-1;
+  for (i=0;i<16;i=i+1)
+   {
+    if(a->enfants[i] != NULL  && a->enfants[i]->score_fin > max)
+     {max = a->enfants[i]->score_fin;
+      res = a->enfants[i]->carte_nous;}
+   }
+ return res;
+}
+
+void free_arbre(arbre *a)
+{
+ int i;
+ if (a == NULL)
+     return;
 
  for (i=0;i<16;i=i+1)
-   free_arbre(&(a->enfants[i]));
+   {if (a->enfants[i]!=NULL)
+     free_arbre(a->enfants[i]);}
 
  free(a);
- free(addr_a);
 return;
 }
 
@@ -92,8 +128,11 @@ int main ()
 {
  int tab1[4]= {1,2,3,4};
  int tab2[4] = {1,2,3,4};
+ int carte_a_joue;
  arbre *a = init_arbre(1,2,tab1,tab2);
  init_score(a,0);
- free_arbre(&a);
+ carte_a_joue = reponce(a);
+ printf("%d",carte_a_joue);
+ free_arbre(a);
 return 0;
 }
